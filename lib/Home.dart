155c627167882a157dc35608b4afd238f2e0dda7
masudrr60 <masudrr60@gmail.com>
2025-07-20 01:29:36 +0600

    Mini to do

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'UserInfo.dart';
 class Home extends StatefulWidget {
@@ -10,6 +9,27 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List<String> _task =[];
   TextEditingController _taskControler = TextEditingController();
+
+
+  _addTask(){
+    if(_taskControler.text.isNotEmpty){
+      setState(() {
+        _task.add(_taskControler.text);
+        _taskControler.clear();
+      });
+    }
+  }
+  _removeTask(int index){
+    setState(() {
+      _task.removeAt(index);
+    });
+  }
+  _removeall(){
+    setState(() {
+      _task.clear();
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,29 +44,36 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             TextField(
+              controller: _taskControler,
               decoration: InputDecoration(
                 hintText: "Enter your task",
                 border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.add),
+                suffixIcon:IconButton(
+                    onPressed:()=> _addTask(),
+                    icon:Icon(Icons.add)),
               ),
             ),
             SizedBox(height: 20,),
-
             Expanded(
-              child: ListView.builder(itemCount: 10,
+              child: ListView.builder(
+                itemCount: _task.length,
                 itemBuilder: (context, index) {
                 return Card(
                   child: ListTile(
-                    title:Text("I have to do this at any time "),
-                    trailing: Icon(Icons.delete),
-                  ),
+                    title:Text(_task[index]),
+                    trailing:IconButton(
+                      onPressed: ()=> _removeTask(index),
+                      icon:Icon(Icons.delete),))
                 );
                 },
               ),
             )
           ],
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:()=> _removeall(),
+        child:Icon(Icons.delete,color: Colors.red),),
     );
   }
 }
